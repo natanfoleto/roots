553feae4c12e78a449543c1aed19ff57dd33164f
Vinicius Faleiros <42284565+vinicius2616@users.noreply.github.com>
2020-11-17 21:55:11 -0300

    alt

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -5,10 +5,11 @@ import Product from '../app/models/Product';
 import Categorie from '../app/models/Categories';
 import Photo from '../app/models/Photo';
 import Provider from '../app/models/Provider';
+import Client from '../app/models/Clients';
 
 import config from '../config/database';
 
-const models = [User, Product, Categorie, Photo, Provider];
+const models = [User, Product, Categorie, Photo, Provider, Client];
 
 class Database {
   constructor() {
